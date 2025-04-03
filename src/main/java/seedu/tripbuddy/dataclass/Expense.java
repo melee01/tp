b75050364b188cc7c805213c3fd95947fc9554fd
noradazaperez <65602872+ndaza03@users.noreply.github.com>
2025-04-03 12:50:36 +0800

    fix

--- src/main/java/seedu/tripbuddy/dataclass/Expense.java
+++ src/main/java/seedu/tripbuddy/dataclass/Expense.java
@@ -7,14 +7,12 @@ import java.time.format.DateTimeFormatter;
  * Stores info of a travel expense.
  * */
 public class Expense {
-
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private String name;
     private double amount;
     private String category;
     private LocalDateTime dateTime;
-
-    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
+    
     public Expense(String name, double amount) {
         this.name = name;
         this.amount = amount;
