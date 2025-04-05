3420a3f3d8a76bc73b09854aaba896c3382090bb
terryzhang1015 <terryzhang1015@outlook.com>
2025-04-05 14:38:35 +0800

    Fix inversed currency ratios

--- src/main/java/seedu/tripbuddy/dataclass/Currency.java
+++ src/main/java/seedu/tripbuddy/dataclass/Currency.java
@@ -1,22 +1,22 @@
 package seedu.tripbuddy.dataclass;
 public enum Currency {
-    MYR("MYR", 0.3025),
-    PHP("PHP", 0.02338),
+    MYR("MYR", 3.29),
+    PHP("PHP", 42.63),
     SGD("SGD", 1),
-    THB("THB", 0.03951),
-    USD("USD", 1.342 ),
-    EUR("EUR", 1.453 ),
-    JPY("JPY", 0.008953),
-    AUD("AUD", 0.882),
-    CAD("CAD", 0.914),
-    CNY("CNY", 1.009),
-    HKD("HKD", 1.232),
-    INR("INR", 0.0161),
-    NZD("NZD", 0.815),
-    CHF("CHF", 1.502),
-    TWD("TWD", 0.0425),
-    ZAR("ZAR", 0.0712),
-    GBP("GBP", 1.736);
+    THB("THB", 25.52),
+    USD("USD", 0.74),
+    EUR("EUR", 0.68),
+    JPY("JPY", 109.11),
+    AUD("AUD", 1.23),
+    CAD("CAD", 1.06),
+    CNY("CNY", 5.41),
+    HKD("HKD", 5.77),
+    INR("INR", 63.50),
+    NZD("NZD", 1.33),
+    CHF("CHF", 0.64),
+    TWD("TWD", 24.63),
+    ZAR("ZAR", 14.18),
+    GBP("GBP", 0.58);
 
     private final String name;
     private double rate;
